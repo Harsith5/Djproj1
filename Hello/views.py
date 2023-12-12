558b5b6ce009ec9f9c5107e4e881302cd6d00163
Harsith <harsith1308@gmail.com>
2023-12-12 10:29:40 +0530

    Django project host

---
+++ Hello/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django import forms
+from .forms import studentform
+from .models import students
+
+# Create your views here.
+form = studentform()
+my_dict = {"insert_me": "Its lowest ...","sr":"star dust", "n":300,'form':form}
+def index(request):
+    return HttpResponse("<h1> I started learning django and its fun </h1>")
+
+def Hello(request):
+    #return HttpResponse("<h2> I am coming from application urls.py file </h2>")
+    if request.method == "POST":
+         form =studentform(request.POST)
+         if form.is_valid():
+            form.save()
+    else:
+        form = studentform()
+    #return render(request,'update.html',{'form':form}
+    return render(request,'Hello/Home.html',context=my_dict)
+
+def list_view(request):
+    # dictionary for initial data with
+    # field name as keys
+    context={}
+
+    # add the dictionary during initialization
+    context["dataset"]=students.objects.all()
+
+    return render(request,"Hello/list_view.html",context)
